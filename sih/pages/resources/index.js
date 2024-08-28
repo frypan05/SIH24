2319363323d2de1111c54a72d8d395d36bb2a29b
frypan05 <daksharma5804@gmail.com>
2024-08-28 12:05:26 +0530

    hi

--- sih/pages/resources/index.js
+++ sih/pages/resources/index.js
@@ -15,7 +15,7 @@ const Resources = () => {
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#2C2C2C' }}>
       <div style={{ textAlign: 'center' }}>
         <h1 style={{ color: '#fff', marginBottom: '20px' }}>Are you a Student or an Instructor?</h1>
-        <button onClick={() => handleSelection('student')} style={{ marginRight: '10px' }}>Student</button>
+        <button onClick={() => handleSelection('student')} style={{ marginRight: '50px' }}>Student</button>
         <button onClick={() => handleSelection('instructor')}>Instructor</button>
       </div>
     </div>
