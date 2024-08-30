38fb8f56a907711c8de72d3fa2f90845dc2dbe3f
Ayush Dixit <ayushrkl123@gmail.com>
2024-08-31 00:52:30 +0530

    learner add

--- sih/pages/resources/index.js
+++ sih/pages/resources/index.js
@@ -25,14 +25,14 @@ const Resources = () => {
       </button>
       <div className="text-center">
         <h1 className="text-6xl font-bold text-[#2BBCDC] mb-4">
-          Are you a Student or an Instructor?
+          Are you a Learner or an Instructor?
         </h1>
         <div className="flex justify-center mb-4">
           <button
             onClick={() => handleSelection('student')}
             className="bg-[#1c1c1c] text-2xl font-bold text-white py-4 px-6 rounded-md hover:bg-[#1C5E6C] transform hover:scale-110 transition duration-300 ease-in-out"
           >
-            Student
+            Learner
           </button>
           <button
             onClick={() => handleSelection('instructor')}
